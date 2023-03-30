d84be7a78091b3ac89ad223f4ecbf671b42e1a0b
kakandavorever <70788217+kmmaniez@users.noreply.github.com>
2023-03-30 13:47:57 +0700

    Add support for indonesia language (id_ID) (#94)
    * Add support for indonesia language (id_ID)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -12,6 +12,7 @@ import es_ES from '../i18n/es_ES.json' assert { type: 'json' };
 import sv from '../i18n/sv.json' assert { type: 'json' };
 import nl from '../i18n/nl.json' assert { type: 'json' };
 import ru from '../i18n/ru.json' assert { type: 'json' };
+import id_ID from '../i18n/id_ID.json' assert { type: 'json'};
 
 export enum I18nLocals {
   'en' = 'en',
@@ -26,8 +27,9 @@ export enum I18nLocals {
   'pt_br' = 'pt_br',
   'es_ES' = 'es_ES',
   'sv' = 'sv',
-  'ru' = 'ru'
-}
+  'ru' = 'ru',
+  'id_ID' = 'id_ID',
+};
 
 export const i18n = {
   en,
@@ -42,6 +44,7 @@ export const i18n = {
   vi_VN,
   es_ES,
   sv,
+  id_ID,
   nl,
   ru
 };
@@ -60,7 +63,8 @@ export const I18N_CONFIG_ALIAS: { [key: string]: string[] } = {
   en: ['en', 'English', 'english'],
   es_ES: ['es_ES', 'Spanish', 'español'],
   sv: ['sv', 'Swedish', 'Svenska'],
-  ru: ['ru', 'Russian', 'русский']
+  ru: ['ru', 'Russian', 'русский'],
+  id_ID: ['id_ID', 'Bahasa', 'bahasa'],
 };
 
 export function getI18nLocal(value: string): string | boolean {
